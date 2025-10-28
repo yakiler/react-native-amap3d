d2c396e47a6a4be29ca5ee39693e077db90211b3
yakiler655 <yakiler655@gmail.com>
2025-10-28 10:12:05 +0800

    fix: Fix the path in react native config

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       ios: { project: "lib/ios/react-native-amap3d.podspec" },
-      android: { sourceDir: "lib/android" },
+      android: { sourceDir: "./lib/android" },
     },
   },
   dependencies: {
